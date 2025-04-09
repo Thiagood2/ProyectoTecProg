eb99e405bb18e437d356b1987e051d67c0b13207
Thiagood2 <thiagomartinezz164@gmail.com>
2025-04-09 18:37:48 -0300

    first commit

---
+++ Venta_class.py
@@ -0,0 +1,3 @@
+class Venta:
+    def __init__(self,fecha_hora):
+        self.fecha_hora = fecha_hora
---
+++ argentur_class.py
@@ -0,0 +1,7 @@
+
+class Angertur:
+    def __init__(self, es_activo):
+        self.sistema_activo = es_activo
+        
+
+
