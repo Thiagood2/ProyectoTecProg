4c97aa1b567df34abdcfaa5911755f0e092dece7
Thiagood2 <captaincloock@gmail.com>
2025-04-19 14:35:48 -0300

    Nueva funcionalidad de Mostrar Servicios

--- transporte.py
+++ transporte.py
@@ -1,15 +1,66 @@
 from datetime import datetime
 from viajes import Unidad, Itinerario
 
-class Argentur:
-    def __init__(self, sistema_activo:float):
-        self.sistema_activo = sistema_activo
+
 
 class Servicio:
-    def __init__(self, unidad:Unidad,fecha_partida:datetime, fecha_llegada:datetime, calidad:str,precio:float, itinerario:Itinerario):
+    def __init__(self, id_servicio:int, unidad:Unidad,fecha_partida:datetime, fecha_llegada:datetime, calidad:str,precio:float, itinerario:Itinerario):
+        self.id = id_servicio
         self.unidad = unidad
         self.fecha_partida = fecha_partida
         self.fecha_llegada = fecha_llegada
         self.calidad = calidad
         self.precio = precio
         self.itinerario = itinerario
+
+    def obtener_id(self):
+        return self.id
+    def obtener_unidad(self):
+        return self.unidad
+    def obtener_fecha_partida(self):
+        return self.fecha_partida
+    def obtener_fecha_llegada(self):
+        return self.fecha_llegada
+    def obtener_calidad(self):
+        return self.calidad
+    def obtener_precio(self):
+        return self.precio
+
+    def mostrar_itinerario(self):
+        print('Itinerario: ')
+        print(f'''
+            - Origen: {self.itinerario.obtener_ciudad_origen()}
+            - Paradas Itermedias: {self.itinerario.obtener_paradas()}
+            - Destino: {self.itinerario.obtener_ciudad_destino()}
+            '''
+        )
+
+
+
+class Argentur:
+    def __init__(self, sistema_activo: float):
+        self.sistema_activo = sistema_activo
+        self.servicios = []
+
+    def agregar_servicio(self, servicio: Servicio):
+        self.servicios.append(servicio)
+
+    def mostrar_servicios(self):
+
+        print('===================================')
+        print('SERVICIOS DISPONIBLES')
+        print('===================================')
+        for servicio in self.servicios:
+    #Agregar logica de Asientos disponibles en (UNIDAD)
+            print(f'Servicio ID: {servicio.obtener_id()}')
+            servicio.mostrar_itinerario()
+            print(f'''
+            - Calidad del Servicio: {servicio.obtener_calidad()}
+            - Fechas disponibles: 
+                - Salida: {servicio.obtener_fecha_salida()}
+                - LLegada: {servicio.obtener_fecha_llegada()}
+        ---------------------------------------------------------''')
+
+
+
+
--- viajes.py
+++ viajes.py
@@ -1,11 +1,18 @@
-from typing import List
-from transporte import Servicio
+
 
 class Ciudad:
     def __init__(self,codigo:str, nombre:str,provincia:str):
         self.codigo = codigo
         self.nombre_ciudad = nombre
         self.nombre_provincia = provincia
+
+    def obtener_codigo(self):
+        return self.codigo
+    def obtener_nombre_ciudad(self):
+        return self.nombre_ciudad
+    def obtener_provincia(self):
+        return self.nombre_provincia
+
 
 class Itinerario:
     def __init__(self, ciudad_origen: Ciudad, ciudad_destino: Ciudad):
@@ -16,10 +23,26 @@ class Itinerario:
     def agregar_paradas(self,ciudad:Ciudad):
         self.paradas_itermedias.append(ciudad)
 
+    def obtener_ciudad_origen(self):
+        return self.ciudad_origen
+
+    def obtener_ciudad_destino(self):
+        return self.ciudad_destino
+
+    def obtener_paradas(self):
+        return self.paradas_itermedias
+
+
+
+
+
 
 class Unidad:
     def __init__(self,patente:str):
         self.patente = patente
+        self.asientos = []
+
+
 
 class Asiento:
     def __init__(self,numero:int,ocupado:bool):
